a9e745ed9859cd036a21f7b15d5fcabe46345bc6
Molly Yang <mollyisgreen@gmail.com>
2014-03-11 17:32:39 -0400

    stripe redirecting properly. now to check if charging

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,8 @@ exports.index = function(req, res){
   res.render('index.html', { title: 'Express' });
 };
 
-
-exports.autocomplete = function(req, res){
-  res.render('autocomplete.html', { title: 'Express' });
+exports.harvard = function(req, res){
+  res.render('harvard.html', { title: 'Express' });
 };
 
 
